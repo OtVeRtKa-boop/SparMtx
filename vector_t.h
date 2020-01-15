088034fb97cc07dca88b5a7d55c46ac0d89005df
OtVeRtKa-boop <54847151+OtVeRtKa-boop@users.noreply.github.com>
2020-01-15 11:31:18 +0400

    Update vector_t.h

--- vector_t.h
+++ vector_t.h
@@ -5,20 +5,27 @@
 template<typename T>
 class vector_t {
 public:
-	/*оператор вывода*/
-	friend 	std::ostream & operator<<(std::ostream & os, const vector_t<T>& v);
-		
-	vector_t() : size(0) {
-		this->entities = nullptr;
-	}
-
 	vector_t(const int size_) : size(size_) {
 		this->entities = new double[size];
 		for (unsigned i = 0; i < this->size; i++)
 			entities[i] = 1;
 	}
+	vector_t() : size(0) {
+		this->entities = nullptr;
+	}
+	template<typename TT>
+	friend 	std::ostream & operator<<(std::ostream & os, const vector_t<TT>& v);
 private:
 	unsigned size{ 0 };
 	T* entities;
 };
 
+template<typename TT>
+friend 	std::ostream & operator<<(std::ostream & os, const vector_t<TT>& v){
+	for (int i = 0; i < v.size; i++)
+		os << v.entities[i] << " ";
+	os << endl;
+	return os;
+}
+
+	
